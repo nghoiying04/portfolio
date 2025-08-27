881c61af90513b6d5b6178166eb413a6c2057880
Rainie Ng <nghoiying04@gmail.com>
2025-08-27 15:32:06 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   swcMinify: false,
   output: 'export',
   reactStrictMode: true
